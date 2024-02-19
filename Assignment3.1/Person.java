a1add274fa027742e36dd42edfb86f7e0aacdb41
RhyLeeSprague <156242808+RhyLeeSprague@users.noreply.github.com>
2024-02-19 13:11:50 -0600

    commit

--- Assignment3.1/Person.java
+++ Assignment3.1/Person.java
@@ -5,30 +5,31 @@ public class Person {
     private String name;
     private int age;
 
+    // Default constructor 
     public Person() {
 
     }
-
+    //Grabs instance of name and sets it as name allows change later on.
     public void setName(String name){
         this.name = name;
     }
-
+    //Grabs instance of age and sets it as age allows change later on.
     public void setAge(int age){
         this.age = age;
     }
-
+    //Grabs name from specific instance.
     public String getName(){
-        return name;
+        return this.name;
     }
-
+    //Grabs age from specific instance.
     public int getAge(){
-        return age;
+        return this.age;
     } 
-
+    //Displays name and age of specific instance of person.
     public void displayDetails(){
         System.out.println("--- Person Details ---");
-        System.out.println("Name: " + name);
-        System.out.println("Age " + age);
+        System.out.println("Name: " + this.name);
+        System.out.println("Age " + this.age);
     }
 
 }
