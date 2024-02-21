c66d47f43ae099a062222a057099cabdf439d65c
RhyLeeSprague <156242808+RhyLeeSprague@users.noreply.github.com>
2024-02-21 13:48:59 -0600

    commit

--- Assignment3.1/main.java
+++ Assignment3.1/main.java
@@ -1,21 +1,24 @@
 public class main{
 public static void main(String[] args) {
- Job job = new Job();
- Person person = new Person();
- HardSkill hskill = new HardSkill();
+    //Creates new instances of job person and hardskill classes.
+    Job job = new Job();
+    Person person = new Person();
+    HardSkill hskill = new HardSkill();
 
- job.setTitle("Employee");
- job.setExperience(8);
- hskill.setTitle("Programming");
- hskill.setLevel("Intermediate");
- person.setName("Billy Joe");
- person.setAge(37);
+    //Sets the variables such as titles, experiemces, levels, name, and age for the instances.
+    job.setTitle("Employee");
+    job.setExperience(8);
+    hskill.setTitle("Programming");
+    hskill.setLevel("Intermediate");
+    person.setName("Billy Joe");
+    person.setAge(37);
 
- person.displayDetails();
- System.out.println("");
- job.displayDetails();
- System.out.println("");
- hskill.displayDetails();
+    //Calls the display methods from each class and displays them
+    person.displayDetails();
+    System.out.println("");
+    job.displayDetails();
+    System.out.println("");
+    hskill.displayDetails();
 
 }
 }
