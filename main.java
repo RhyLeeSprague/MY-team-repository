cb70d6436118c516abb820bfb962fcf5010db44c
kiera taylor <156949551+ghostlyspooky@users.noreply.github.com>
2024-02-21 13:13:04 -0600

    Update main.java

--- main.java
+++ main.java
@@ -1,6 +1,21 @@
-import java.util.Scanner;
-
+public class main{
 public static void main(String[] args) {
-    static Scanner scn = new Scanner(System.in);
+ Job job = new Job();
+ Person person = new Person();
+ HardSkill hskill = new HardSkill();
 
+ job.setTitle("Employee");
+ job.setExperience(8);
+ hskill.setTitle("Programming");
+ hskill.setLevel("Intermediate");
+ person.setName("Billy Joe");
+ person.setAge(37);
+
+ person.displayDetails();
+ System.out.println("");
+ job.displayDetails();
+ System.out.println("");
+ hskill.displayDetails();
+
+}
 }
