fb01bda779e744607adf5ce8991295f634e4bf66
RhyLeeSprague <156242808+RhyLeeSprague@users.noreply.github.com>
2024-04-01 09:10:51 -0500

    commit

--- TempArrays.java
+++ TempArrays.java
@@ -1,3 +1,25 @@
+import java.util.ArrayList;
+
 public class TempArrays {
+    ArrayList<Object> myEmployees = new ArrayList<>();
+    ArrayList<Object> myManagers = new ArrayList<>();
+    ArrayList<Object> Evalutions = new ArrayList<>();
+    ArrayList<Object> jobHistory = new ArrayList<>();
+
+    //Skills are multidimensonal arrays to contain the skill then the level of said skill.
+    String[][] softSkills = new String[250][250];
+    String[][] hardSkills = new String[250][250];
+    String[][][] emergencyContact = new String[250][250][250];
+    String[] gifts = new String[250];
+    
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String emailAddress;
+    private String passwords;
+
+
+
+
 
 }
