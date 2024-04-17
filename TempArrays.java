e7a8ed654d1010f9cc6581d407e3831d42848052
RhyLeeSprague <156242808+RhyLeeSprague@users.noreply.github.com>
2024-04-17 13:12:35 -0500

    commit

--- TempArrays.java
+++ TempArrays.java
@@ -1,9 +1,11 @@
 import java.util.ArrayList;
 
 public class TempArrays {
-    ArrayList<Object> myEmployees = new ArrayList<>();
-    ArrayList<Object> myManagers = new ArrayList<>();
+    // An object array list to contain an employee that holds their demographics info and accessLevel
+    ArrayList<Object[]> myEmployees = new ArrayList<>();
+    // An object array list to contain an evaluation over an employee's performance
     ArrayList<Object> Evalutions = new ArrayList<>();
+    // An object array list to contain a jobHistory overview on an employee's past jobs.
     ArrayList<Object> jobHistory = new ArrayList<>();
 
     //Skills are multidimensonal arrays to contain the skill then the level of said skill.
@@ -20,8 +22,27 @@ public class TempArrays {
     private String emailAddress;
     private String passwords;
 
+    //edit
+    public void editArrayObject(int arrayIndex, int objectIndex, Object newObject) {
+        if (arrayIndex >= 0 && arrayIndex < myEmployees.size()) {
+            if (objectIndex >= 0 && objectIndex < myEmployees.get(arrayIndex).length) {
+                myEmployees.get(arrayIndex)[objectIndex] = newObject;
+            } else {
+                System.err.println("Object's index out of bounds");
+            }
+        } else {
+            System.err.println("The chosen index is out of bounds");
+        }
+    }
+
+    //add
+
+    //and delete methods
 
 
+    public void innerArrays(Object[] newArray) {
+        myEmployees.add(newArray);
+    }
 
 
 }
